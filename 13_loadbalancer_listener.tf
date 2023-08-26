3ef197e8d53ce2716ec1a59041c8610edf68af8d
Ebs012 <134578012+Ebs012@users.noreply.github.com>
2023-08-26 17:27:18 +0100

    Update 13_loadbalancer_listener.tf

--- 13_loadbalancer_listener.tf
+++ 13_loadbalancer_listener.tf
@@ -4,6 +4,7 @@ resource "aws_lb_listener" "example" {
   port              = "80"
   protocol          = "HTTP"
 
+#default action
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb-target-group.arn
